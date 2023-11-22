0053db08e9745009c29962cba4bf3e286ee141f3
Alex Garrett-Smith <alex@codecourse.com>
2023-11-22 13:02:47 +0000

    Episode 08

--- app/Livewire/SpecificUpload.php
+++ app/Livewire/SpecificUpload.php
@@ -3,12 +3,16 @@
 namespace App\Livewire;
 
 use Illuminate\Http\UploadedFile;
+use Illuminate\Support\Str;
 use Livewire\Component;
 
 class SpecificUpload extends Upload
 {
     public function onSuccess(UploadedFile $file)
     {
-        dd($file);
+        auth()->user()->files()->create([
+            'file_name' => $file->getClientOriginalName(),
+            'file_path' => $file->storeAs('uploads', Str::uuid())
+        ]);
     }
 }
